6a344dbc790848f3d3dabda5ba9d3acd2e5c97ec
Kyriakos Chatzidimitriou <kyrcha@gmail.com>
2014-07-01 15:34:53 +0300

    client updates

--- wsat-client/app/scripts/controllers/service.js
+++ wsat-client/app/scripts/controllers/service.js
@@ -1,59 +1,80 @@
 'use strict';
 
 angular.module('angClientApp')
-  .controller('ServiceCtrl', function ($scope, $http, $stateParams) {
+	.controller('ServiceCtrl', function ($scope, $http, $stateParams) {
 
-  	var getRootNodesScope = function() {
-      return angular.element(document.getElementById("tree-root")).scope();
-    };
+		var accountId = 1;
+  	
+  		var getRootNodesScope = function() {
+      		return angular.element(document.getElementById("tree-root")).scope();
+    	};
 
-	  var accountId = 2;
-	  $http.get("http://localhost:8080/wsAnnotationTool/api/account/" + accountId + "/RESTService/" + $stateParams.serviceid)
-	  .success(function(data, status, headers, config) {
-	    $scope.service = data;
+		$http.get("http://localhost:8080/wsAnnotationTool/api/account/" + accountId + "/RESTService/" + $stateParams.serviceid)
+	  	.success(function(data, status, headers, config) {
+			$scope.service = data;
 
-	    $scope.list = [ {
-	    	"id" : $scope.service.RESTServiceId,
-	    	"type": "base",
-	    	"title": "Base",
-	    	"link": "/services/" + $stateParams.serviceid,
-	    	"collapsed": true,
-	    	"items": []
-	    }];
+		    $scope.list = [ {
+		    	"id" : $scope.service.RESTServiceId,
+		    	"type": "base",
+		    	"title": "Base",
+		    	"link": "/services/" + $stateParams.serviceid + "/base",
+		    	"expandLink": "http://localhost:8080/wsAnnotationTool/api/account/" + accountId + "/RESTService/" + $stateParams.serviceid + "/resource",
+		    	"collapsed": true,
+		    	"items": []
+		    }];
 
-	    // var scope = getRootNodesScope();
-	    // scope.collapseAll();	
-	    
-	  });
+	  	});
 
-	  $scope.play = function(scope) {
-	  	console.log(scope);
-	  	var nodeData = scope.$modelValue;
-	  	console.log(nodeData);
-	  	if(!(nodeData.items.length > 0)) {
-	  		if(nodeData.type == "base") {
-				$http.get("http://localhost:8080/wsAnnotationTool/api/account/" + accountId + "/RESTService/" + $stateParams.serviceid + "/resource")
-	  			.success(function(data, status, headers, config) {
-	    			var resources = data.linkList;
-	    			for(var i in resources) {
-	    				var id = resources[i].uri.charAt(resources[i].uri.length - 1);
-	    				var obj = {
-							"id" : id,
-							"type": "resource",
-							"title": resources[i].rel,
-	    					"link": "/services/" + $stateParams.serviceid + "/resources/" + id,
-	    					"collapsed": true,
-	    					"items": []
-	    				};
-	    				nodeData.items.push(obj);
-	    			}
-	    		});
-	  		}
-
-	  	}
-	  	nodeData.collapsed = !nodeData.collapsed
-      	scope.toggle();
-      };
+		$scope.play = function(scope) {
+			console.log(scope);
+			var nodeData = scope.$modelValue;
+			console.log(nodeData);
+			if(!(nodeData.items.length > 0)) {
+				if(nodeData.type == "base") {
+					$http.get(nodeData.expandLink)
+					.success(function(data, status, headers, config) {
+						var resources = data.linkList;
+						for(var i in resources) {
+							if(resources[i].type == "Child" && resources[i].httpVerb == "GET") {
+								var id = resources[i].uri.substring(resources[i].uri.lastIndexOf("/") + 1, resources[i].uri.length);
+								var obj = {
+									"id" : id,
+									"type": "resource",
+									"title": (resources[i].rel === "null" ? "Resource " + id : resources[i].rel),
+									"link": "/services/" + $stateParams.serviceid + "/resources/" + id,
+									"expandLink": nodeData.expandLink + "/" + id,
+									"collapsed": true,
+									"items": []
+								};
+								nodeData.items.push(obj);
+							}
+						}
+					});
+				} else if(nodeData.type == "resource") {
+					$http.get(nodeData.expandLink+"/RESTMethod")
+					.success(function(data, status, headers, config) {
+						var resources = data.linkList;
+						for(var i in resources) {
+							if(resources[i].type == "Child") {
+								var id = resources[i].uri.substring(resources[i].uri.lastIndexOf("/") + 1, resources[i].uri.length);
+								var obj = {
+									"id" : id,
+									"type": "method",
+									"title": resources[i].rel,
+									"link": nodeData.link + "/methods/" + id,
+									"expandLink": nodeData.expandLink + "/RESTMethod/" + id + "/RESTParameter",
+									"collapsed": true,
+									"items": []
+								};
+								nodeData.items.push(obj);
+							}
+						}
+					});
+				}
+			}
+			nodeData.collapsed = !nodeData.collapsed
+			scope.toggle();
+		};
 
  //    $scope.list = [{
 	// 	"id": 1,
--- wsat-client/app/scripts/controllers/services.js
+++ wsat-client/app/scripts/controllers/services.js
@@ -1,13 +1,20 @@
 'use strict';
 
 angular.module('angClientApp')
-    .controller('ServicesCtrl', function ($scope, $http) {
-          var accountId = 2;
-          $http.get("http://localhost:8080/wsAnnotationTool/api/account/" + accountId + "/RESTService")
-          .success(function(data, status, headers, config) {
-            $scope.services = data.linkList;
-            for(var i in $scope.services) {
-              $scope.services[i]["id"] = $scope.services[i].uri.charAt($scope.services[i].uri.length - 1);
+	.controller('ServicesCtrl', function ($scope, $http) {
+		var accountId = 1;
+		$http.get("http://localhost:8080/wsAnnotationTool/api/account/" + accountId + "/RESTService")
+		.success(function(data, status, headers, config) {
+		$scope.services = [];
+			for(var i in data.linkList) {
+				if(data.linkList[i].type == "Child") {
+					console.log(data.linkList[i]);
+					var id = data.linkList[i].uri.substring(data.linkList[i].uri.lastIndexOf("/") + 1, data.linkList[i].uri.length);
+					$scope.services.push({
+						"id": id,
+						"rel": data.linkList[i].rel
+					});
+				}
             }
           });
       });
