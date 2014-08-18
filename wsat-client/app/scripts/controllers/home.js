c01e00f317d2d767c4cf74943daa1bc8dda7ad39
Kyriakos Chatzidimitriou <kyrcha@gmail.com>
2014-08-18 10:18:11 +0300

    Changes

--- wsat-client/app/scripts/controllers/home.js
+++ wsat-client/app/scripts/controllers/home.js
@@ -1,10 +1,22 @@
 'use strict';
 
 angular.module('angClientApp')
-  .controller('HomeCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('HomeCtrl', function ($scope, $http) {
+
+  	$scope.search = function() {	
+  		$http.get("http://localhost:8080/wsAnnotationTool/api/algoSearch?searchRESTResource=" + $scope.searchRESTResource + "&searchRESTService=" + $scope.searchRESTService + "&searchRESTMethod=" + $scope.searchRESTMethod + "&searchRESTParameter=" + $scope.searchRESTParameter + "&searchSOAPService=" + $scope.searchSOAPService + "&searchSOAPOperation=" + $scope.searchSOAPOperation + "&searchSOAPParameter=" + $scope.searchSOAPParameter + "&searchKeyword="  + $scope.searchBox)
+	  	.success(function(data, status, headers, config) {
+	  		if(angular.isArray(data.linkList)) {
+	  			console.log("there");
+	  			$scope.results = data.linkList;	
+	  		} else {
+	  			console.log("here");
+	  			var list = [];
+	  			list.push(data.linkList)
+	  			$scope.results = list;	
+	  		}
+	  		console.log($scope.results);
+	  	});
+  	}
+
   });
